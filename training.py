4908cfee77a2b0a065242861c207962320de41c7
Alejandro <armentaayalaalejandro@gmail.com>
2026-02-24 18:20:37 -0600

    it is learning

--- training.py
+++ training.py
@@ -5,6 +5,8 @@ from functools import partial
 
 import torch
 
+import torch.nn.functional as F
+
 from torch.optim import AdamW
 
 from torch.utils.data import random_split, DataLoader
@@ -98,21 +100,41 @@ optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
 
 scheduler = get_linear_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=total_steps)
 
-def train_epoch(model, dataloader, optimizer, device):
+def train_epoch(model, dataloader, optimizer, device, clip_norm=1.0):
+
     model.train()
 
     progress = tqdm(dataloader, desc='training epoch')
 
     for input_ids, attn_mask in progress:
+        
         input_ids = input_ids.to(device)
-        #print("before", input_ids.shape)
 
         inputs = input_ids[:,:-1]
+
         targets = input_ids[:,1:]
 
         optimizer.zero_grad(set_to_none=True)
+        
         logits = model(inputs)
 
+        a = logits.view(-1, logits.size(-1))
+        
+        b = targets.reshape(-1)
+
+        loss = F.cross_entropy(a, b, ignore_index=tokenizer.pad_token_id)
+
+        loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=clip_norm)
+
+        optimizer.step()
+
+        scheduler.step()
+
+        progress.set_postfix(loss=f"{loss.item():.4f}")
+
+        
 
 
 
