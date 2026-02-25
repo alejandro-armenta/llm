00dc25cf6393bcedc159629167bb1c0c9ba83d22
Alejandro <armentaayalaalejandro@gmail.com>
2026-02-24 19:39:31 -0600

    saved best model

--- training.py
+++ training.py
@@ -2,7 +2,6 @@ from transformers import AutoTokenizer, get_linear_schedule_with_warmup
 
 from functools import partial
 
-
 import torch
 
 import torch.nn.functional as F
@@ -11,12 +10,10 @@ from torch.optim import AdamW
 
 from torch.utils.data import random_split, DataLoader
 
-
-from utils import TextDataset, collate_fn
+from utils import TextDataset, collate_fn, train_epoch
 
 from gptconfig import GPTConfig, GPT
 
-from tqdm import tqdm
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
@@ -56,24 +53,6 @@ collate = partial(
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate, num_workers=10)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, collate_fn=collate)
 
-"""
-input_ids, attn_mask = next(iter(train_loader))
-
-#ahora es el mayor de todos truncado
-print(input_ids.shape)
-print(attn)
-
-zero gradients each batch
-
-Forward
-
-Compute Loss
-
-Backward pass
-
-update weights
-"""
-
 config = GPTConfig(
     vocab_size=50257,
     max_seq_len=128,
@@ -91,7 +70,9 @@ model.to(device=device)
 print(f"Parameters: {sum(p.numel() for p in model.parameters()):,}")
 
 learning_rate = 3e-4
+
 num_warmup_steps = 100
+
 num_epochs = 3
 
 total_steps = len(train_loader) * num_epochs
@@ -100,43 +81,66 @@ optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
 
 scheduler = get_linear_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=total_steps)
 
-def train_epoch(model, dataloader, optimizer, device, clip_norm=1.0):
 
-    model.train()
+def evaluate(model, dataloader, device):
+    model.eval()
 
-    progress = tqdm(dataloader, desc='training epoch')
+    total_loss = 0
+    total_tokens = 0
 
-    for input_ids, attn_mask in progress:
-        
+    for input_ids, attn_mask in dataloader:
         input_ids = input_ids.to(device)
 
         inputs = input_ids[:,:-1]
 
         targets = input_ids[:,1:]
 
-        optimizer.zero_grad(set_to_none=True)
-        
         logits = model(inputs)
 
         a = logits.view(-1, logits.size(-1))
         
         b = targets.reshape(-1)
 
-        loss = F.cross_entropy(a, b, ignore_index=tokenizer.pad_token_id)
-
-        loss.backward()
+        loss = F.cross_entropy(
+            a, 
+            b, 
+            ignore_index=tokenizer.pad_token_id,
+            reduction='sum'
+            )
         
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=clip_norm)
+        mask = (targets != tokenizer.pad_token_id)
 
-        optimizer.step()
+        total_loss += loss.item()
+        total_tokens += mask.sum().item()
 
-        scheduler.step()
+    #print(total_loss)
+    #print(total_tokens)
+    
+    mean_loss_per_token = total_loss/total_tokens
 
-        progress.set_postfix(loss=f"{loss.item():.4f}")
-
-        
+    return mean_loss_per_token
 
 
+def save_model():
+    pass
 
+best_val_loss = float('inf')
 
-train_epoch(model=model, dataloader=train_loader, optimizer=optimizer, device=device)
+for e in range(num_epochs):
+
+    train_loss = train_epoch(model=model, dataloader=train_loader, optimizer=optimizer, scheduler=scheduler, tokenizer=tokenizer, device=device, clip_norm=1.0)
+
+    val_loss = evaluate(model=model, dataloader=val_loader, device=device)
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save({
+            'epoch':e,
+            'model_state_dict':model.state_dict(),
+            'optimizer_state_dict':optimizer.state_dict(),
+            'scheduler_state_dict':scheduler.state_dict(),
+            'train_loss':train_loss,
+            'val_loss':val_loss
+        }, f='best_model.pt')
+
+print(best_val_loss)
--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
-from torch.utils.data import Dataset, DataLoader, random_split
+import torch
+
+from torch.utils.data import Dataset
+
+import torch.nn.functional as F
+
+from tqdm import tqdm
 
 class TextDataset(Dataset):
     def __init__(self, text, chunk_size=128):
@@ -25,3 +31,44 @@ def collate_fn(batch, tokenizer, max_length=128):
         )
     
     return encoded['input_ids'], encoded['attention_mask']
+
+
+def train_epoch(model, dataloader, optimizer, scheduler, tokenizer, device, clip_norm=1.0):
+
+    model.train()
+
+    progress = tqdm(dataloader, desc='training epoch')
+
+    epoch_loss = 0
+
+    for input_ids, attn_mask in progress:
+        
+        input_ids = input_ids.to(device)
+
+        inputs = input_ids[:,:-1]
+
+        targets = input_ids[:,1:]
+
+        optimizer.zero_grad(set_to_none=True)
+        
+        logits = model(inputs)
+
+        a = logits.view(-1, logits.size(-1))
+        
+        b = targets.reshape(-1)
+
+        loss = F.cross_entropy(a, b, ignore_index=tokenizer.pad_token_id)
+
+        loss.backward()
+        
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=clip_norm)
+
+        optimizer.step()
+
+        scheduler.step()
+
+        progress.set_postfix(loss=f"{loss.item():.4f}")
+
+        epoch_loss += loss.item()
+
+    return epoch_loss / len(dataloader)
