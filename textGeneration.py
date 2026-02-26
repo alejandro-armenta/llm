58d976ea5dfb283b309e8690002d56cee7e262d1
Alejandro <armentaayalaalejandro@gmail.com>
2026-02-25 19:27:12 -0600

    tested best model

--- textGeneration.py
+++ textGeneration.py
@@ -8,15 +8,15 @@ from gptconfig import GPTConfig, GPT
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-chech = torch.load('best_model.pt', map_location=device)
+chech = torch.load('best_model_big.pt', map_location=device)
 
 print(chech['val_loss'])
 
-"""
-config = GPTConfig()
-a = GPT(config=config)
-"""
 
+config = GPTConfig()
+model = GPT(config=config)
+
+"""
 config = GPTConfig(
     vocab_size=50257,
     max_seq_len=128,
@@ -28,6 +28,7 @@ config = GPTConfig(
 )
 
 model = GPT(config=config)
+"""
 
 
 model.to(device)
